6ca098f1c9e1c30aa3f265ff8f10663123c933fc
RSSaltea <144387871+RSSaltea@users.noreply.github.com>
2026-02-16 15:39:14 +0000

    Update npcs.py

--- wildygame/npcs.py
+++ wildygame/npcs.py
@@ -5,6 +5,8 @@ NPCS: List[Tuple[str, int, int, int, str, int, int]] = [
     ("Revenant goblin", 50, 1, 1, "revenant", 3, 1),
     ("Revenant knight", 50, 2, 10, "revenant knight", 5, 3),
     ("Chaos fanatic", 90, 3, 20, "chaos_fanatic", 7, 5),
+    ("Revenant necromancer", 140, 4, 20, "revenant necro", 10, 0),
+    ("Revenant abyssal demon", 140, 4, 35, "revenant demon", 9, 3),
     ("Abyssal Overlord", 280, 4, 35, "overlord", 11, 7),
     ("Lord Valthyros", 250, 4, 35, "valthyros", 6, 12),
     ("Zarveth the Veilbreaker", 420, 5, 45, "veilbreaker", 22, 4),
@@ -41,6 +43,41 @@ NPC_DROPS: Dict[str, Dict[str, Any]] = {
             {"item": "Revenant ether", "min": 10, "max": 80, "chance": "1/2"},
             {"item": "Bracelet of ethereum", "min": 1, "max": 1, "chance": "1/250"},
             {"item": "Viggora's Chainmace", "min": 1, "max": 1, "chance": "1/1000"},
+        ],
+    },
+    "revenant necro": {
+        "coins_range": [0, 2500],
+        "unique": [
+            {"item": "Mysterious key", "min": 1, "max": 1, "chance": "1/10"},
+            {"item": "Revenant Relic Shard", "min": 1, "max": 1, "chance": "1/250"},
+            {"item": "Revenant Totem", "min": 1, "max": 1, "chance": "1/500"},
+        ],
+        "pet": [
+            {"item": "Tiny Revenant", "min": 1, "max": 1, "chance": "1/1000"},
+        ],
+        "loot": [
+            {"item": "Revenant ether", "min": 30, "max": 70, "chance": "1/2"},
+            {"item": "Bracelet of ethereum", "min": 1, "max": 1, "chance": "1/250"},
+            {"item": "Death Guard", "min": 1, "max": 1, "chance": "1/150"},
+            {"item": "Skull Lantern", "min": 1, "max": 1, "chance": "1/150"},
+            {"item": "Viggora's Chainmace", "min": 1, "max": 1, "chance": "1/900"},
+        ],
+    },
+    "revenant demon": {
+        "coins_range": [0, 3000],
+        "unique": [
+            {"item": "Mysterious key", "min": 1, "max": 1, "chance": "1/20"},
+            {"item": "Revenant Relic Shard", "min": 1, "max": 1, "chance": "1/200"},
+            {"item": "Revenant Totem", "min": 1, "max": 1, "chance": "1/450"},
+        ],
+        "pet": [
+            {"item": "Tiny Revenant", "min": 1, "max": 1, "chance": "1/1000"},
+        ],
+        "loot": [
+            {"item": "Revenant ether", "min": 30, "max": 160, "chance": "1/2"},
+            {"item": "Bracelet of ethereum", "min": 1, "max": 1, "chance": "1/250"},
+            {"item": "Abyssal Whip", "min": 1, "max": 1, "chance": "1/200"},
+            {"item": "Viggora's Chainmace", "min": 1, "max": 1, "chance": "1/850"},
         ],
     },
     "chaos_fanatic": {
